54c0e287294f6a451639fff3d079f7e2bd9899c6
Moyrasc <noemisolac@gmail.com>
2023-04-24 09:59:22 +0200

    exercise completed

--- first/src/main/java/com/bosonit/training/first/ControllerTest.java
+++ first/src/main/java/com/bosonit/training/first/ControllerTest.java
@@ -1,12 +1,18 @@
 package com.bosonit.training.first;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 //Especifica a Spring que estamos creando un controlador
 @RestController
 public class ControllerTest {
+    //Hay que especificar la ruta que tiene que escuchar
     @GetMapping("/")
     public String hello(){
         return "Hello World, it's my first project whit Spring Boot";
+    }
+    @GetMapping("/bye/{name}")
+    public String bye(@PathVariable String name){
+        return "Bye: " + name;
     }
 }
